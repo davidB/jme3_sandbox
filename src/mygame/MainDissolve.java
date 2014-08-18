535eb58309768e899c964c985c868ab948436008
David Bernard <david.bernard.31@gmail.com>
2014-08-18 19:13:14 +0200

    enhance dissove demo : * add a backgound image on quad * use 2 geometry box + sphere * fix the normal of backface to view content of dissovling object * adjuste default distance of the camera

--- src/mygame/MainDissolve.java
+++ src/mygame/MainDissolve.java
@@ -1,16 +1,25 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.bounding.BoundingBox;
+import com.jme3.bounding.BoundingSphere;
+import com.jme3.bounding.BoundingVolume;
 import com.jme3.input.ChaseCamera;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
+import com.jme3.material.RenderState;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Quad;
 import com.jme3.scene.shape.Sphere;
+import com.jme3.ui.Picture;
 
 /**
  * test
@@ -26,23 +35,37 @@ public class MainDissolve extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        Geometry pic = new Geometry("pic", new Quad(10f, 10f));
+        pic.setMaterial(assetManager.loadMaterial("Materials/image.j3m"));
+        pic.setLocalRotation(new Quaternion(new float[]{0.0f, 90f * FastMath.DEG_TO_RAD, 0.0f * FastMath.DEG_TO_RAD}));
+        pic.setLocalTranslation(-6.0f, -4.0f, 5.0f);
+        rootNode.attachChild(pic);
         
-        Box shape = new Box(0.5f, 0.5f, 0.5f);
-        //Sphere shape = new Sphere(16, 16, 0.5f);
-        
-        Geometry geom = new Geometry("obj", shape);
+        Node target = new Node("target");
 
-        //Material mat = assetManager.loadMaterial("Materials/matcap_0.j3m");
-        mat = new Material(assetManager, "ShaderBlow/MatDefs/Dissolve/Lighting.j3md");
+        Geometry[] geoms = {
+            new Geometry("obj", new Box(0.5f, 0.5f, 0.5f))
+            , new Geometry("obj", new Sphere(16, 16, 0.5f))
+        };
+        
+        //FIXME: dissolv should une 1.0 * normal for backface
+        //mat = new Material(assetManager, "ShaderBlow/MatDefs/Dissolve/Lighting.j3md");
+        mat = new Material(assetManager, "MatDefs/DLighting.j3md");
         mat.setBoolean("UseMaterialColors", true);
         mat.setColor("Diffuse", ColorRGBA.Pink);
-        //mat.setTexture("DissolveMap", assetManager.loadTexture("Textures/mapcap_0.png"));
         mat.setTexture("DissolveMap", assetManager.loadTexture("Textures/burnMap.png"));
-        mat.setVector2("DissolveParams", new Vector2f(0.5f, 0.0f));
-        
-        geom.setMaterial(mat);
+        mat.setVector2("DissolveParams", new Vector2f(0.0f, 0.0f));
+        mat.getAdditionalRenderState().setFaceCullMode(RenderState.FaceCullMode.Off);
 
-        rootNode.attachChild(geom);
+        for( int i = 0; i < geoms.length; i++) {
+            Geometry g = geoms[i];
+            g.setMaterial(mat);
+            float pos = ((i+0.5f) - 0.5f * geoms.length) * 2.5f;
+            g.setLocalTranslation(Vector3f.UNIT_Z.mult(pos));
+            target.attachChild(g);
+        }
+
+        rootNode.attachChild(target);
         DirectionalLight light = new DirectionalLight();
         light.setDirection(new Vector3f(-1.0f, -0.75f, -0.5f).normalizeLocal());
         rootNode.addLight(light);
@@ -53,16 +76,36 @@ public class MainDissolve extends SimpleApplication {
 //        flyCam.setDragToRotate(true);
 
         flyCam.setEnabled(false);
-        ChaseCamera chaseCam = new ChaseCamera(cam, geom, inputManager);
+        ChaseCamera chaseCam = new ChaseCamera(cam, target, inputManager);
         chaseCam.setInvertVerticalAxis(true);
         chaseCam.setMinVerticalRotation((float)-Math.PI / 2);
+        //TODO calculate de distance from the boundingbox (+ margin) and projection
+        //chaseCam.setDefaultDistance(2.0f + target.getQuantity() * 2.0f);
+        BoundingVolume bv = target.getWorldBound();
+        float d = 0.5f;
+        switch (bv.getType()) {
+        case Sphere:
+            d += ((BoundingSphere)bv).getRadius();
+            System.out.println("sphere :" + d);
+            break;
+        case AABB:
+            BoundingBox bb = ((BoundingBox) bv);
+            d += Math.max(bb.getYExtent(), Math.max(bb.getZExtent(), bb.getXExtent()));
+            System.out.println("AABB :" + d);
+            break;
+        default:
+            d += target.getQuantity();
+            System.out.println("??? :" + d);
+        }
+        chaseCam.setDefaultDistance(d * 2.0f);
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        float ratio = getTimer().getTimeInSeconds() / 2.0f;
-        ratio = ratio - (float) Math.floor(ratio);
-        mat.setVector2("DissolveParams", new Vector2f(ratio, 0.0f));
+        float part = getTimer().getTimeInSeconds() / 3.0f; // full dissolve every 3s
+        float direction = (float)Math.floor(part) % 2f; // 0.0 or 1.0
+        float ratio = part - (float) Math.floor(part);
+        mat.setVector2("DissolveParams", new Vector2f(ratio, direction));
     }
 
     @Override
