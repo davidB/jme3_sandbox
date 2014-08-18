07608b5e5f414a5cb1d57070e804f7647297a4ef
David Bernard <david.bernard.31@gmail.com>
2014-08-18 16:04:38 +0200

    matcap demo

---
+++ src/mygame/MainMatCap.java
@@ -0,0 +1,45 @@
+package mygame;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.texture.Texture;
+
+/**
+ * test
+ * @author normenhansen
+ */
+public class MainMatCap extends SimpleApplication {
+
+    public static void main(String[] args) {
+        MainMatCap app = new MainMatCap();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        //Material mat = assetManager.loadMaterial("Materials/matcap_0.j3m");
+        Material mat = new Material(assetManager, "ShaderBlow/MatDefs/MatCap/MatCap.j3md");
+        mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/mapcap_0.png"));
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+}
