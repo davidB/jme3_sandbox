33bf88a2ff804222fddf8e55d9b42ad7b78d2fa2
David Bernard <david.bernard.31@gmail.com>
2014-08-18 16:33:22 +0200

    use chasecam

--- src/mygame/MainMatCap.java
+++ src/mygame/MainMatCap.java
@@ -1,6 +1,7 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.ChaseCamera;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
@@ -22,15 +23,25 @@ public class MainMatCap extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
+        
         Box b = new Box(1, 1, 1);
         Geometry geom = new Geometry("Box", b);
 
         //Material mat = assetManager.loadMaterial("Materials/matcap_0.j3m");
         Material mat = new Material(assetManager, "ShaderBlow/MatDefs/MatCap/MatCap.j3md");
         mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/mapcap_0.png"));
+        mat.setColor("Multiply_Color", ColorRGBA.Blue);
         geom.setMaterial(mat);
 
         rootNode.attachChild(geom);
+        
+//        flyCam.setEnabled(true);
+//        flyCam.setMoveSpeed(30);
+//        flyCam.setRotationSpeed(10);
+//        flyCam.setDragToRotate(true);
+
+        flyCam.setEnabled(false);
+        ChaseCamera chaseCam = new ChaseCamera(cam, geom, inputManager);
     }
 
     @Override
