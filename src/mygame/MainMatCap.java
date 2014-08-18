dd5b40c23eaa0acbfd059cf85010e6079ebb1336
David Bernard <david.bernard.31@gmail.com>
2014-08-18 16:51:15 +0200

    adjust chaseCam control

--- src/mygame/MainMatCap.java
+++ src/mygame/MainMatCap.java
@@ -4,11 +4,10 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.input.ChaseCamera;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
-import com.jme3.texture.Texture;
+import com.jme3.scene.shape.Sphere;
 
 /**
  * test
@@ -24,8 +23,10 @@ public class MainMatCap extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        Box shape = new Box(0.5f, 0.5f, 0.5f);
+        //Sphere shape = new Sphere(16, 16, 0.5f);
+        
+        Geometry geom = new Geometry("obj", shape);
 
         //Material mat = assetManager.loadMaterial("Materials/matcap_0.j3m");
         Material mat = new Material(assetManager, "ShaderBlow/MatDefs/MatCap/MatCap.j3md");
@@ -42,6 +43,8 @@ public class MainMatCap extends SimpleApplication {
 
         flyCam.setEnabled(false);
         ChaseCamera chaseCam = new ChaseCamera(cam, geom, inputManager);
+        chaseCam.setInvertVerticalAxis(true);
+        chaseCam.setMinVerticalRotation((float)-Math.PI / 2);
     }
 
     @Override
